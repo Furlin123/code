9d36a3164a1c9c0e7e6daf4ced43ec63d8a56225
Furlin <1554653534@qq.com>
2026-07-06 11:58:12 +0800

    test03 with Node

--- test.cpp
+++ test.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
 using namespace std;
 
+struct SLNode{
+    int data;
+    SLNode* next;
+};//Node definition
+
 int main(){
     int a, b;
     cin>>a>>b;
-
-    cout<<"Hello!"<<endl;
     cout<<a+b<<endl;
     return 0;
 }
